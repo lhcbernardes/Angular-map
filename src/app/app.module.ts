5c47cc041c1b17c0fc795b72fe7f9444e33ac6a0
Leandro <leandro.h.bernardes@accenture.com>
2021-02-01 11:46:43 -0300

    biblioteca faltando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { appRoutingModule } from './app.routing';
@@ -20,6 +20,7 @@ import { RatingModule } from 'ngx-bootstrap/rating';
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule,
         ReactiveFormsModule,
         HttpClientModule,
         RatingModule.forRoot(),
